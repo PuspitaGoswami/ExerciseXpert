7a95b6b93d553afc247a9ae701e1b77da48af236
Puspita <puspitagoswami14@gmail.com>
2023-06-17 23:57:34 +0100

    Nav items added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,11 @@ const Navbar = () => {
         <img src={logo} alt='logo' style={{width:'48px', height:'48px', margin:'0 20px'}}/>
         </Link>
       </Stack>
+      <Stack>
+        <Link to='/'>Home</Link>
+        <a href='#exercises' style={{textDecoration:'none', color:'#3A1212'}}>Exercises</a>
+      </Stack>
+
     </div>
   )
 }
