47cc86081aa491863962598f34d8b1bb19a503ec
Puspita <puspitagoswami14@gmail.com>
2023-06-18 20:03:44 +0100

    Button added on Hero sesction

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Box, Stack, Typography} from '@mui/material';
+import {Box, Stack, Typography, Button} from '@mui/material';
 
 const Herobanner = props => {
   return (
     <Box sx={{mt: {lg:'212px', xs:'70px'}, ml:{sm:'50px'}}} position='relative' p='20px'>
       <Typography color='#ff2625' fontWeight='600' fontSize='26px'>Fitness Club</Typography>
       <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'40px'}}}>Sweat, Smile <br/> and Repeat</Typography>
+    <Typography fontSize='20px' lineHeight='35px' >Check out the most effective exercises</Typography>
+    <Button variant='contained' color='error'>Explore Exercise</Button>
     </Box>
   )
 }
