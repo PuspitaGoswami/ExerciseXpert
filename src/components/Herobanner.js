24bb9117cbd107f7a61aa1ddb9719625e61f8365
Puspita <puspitagoswami14@gmail.com>
2023-06-18 23:14:54 +0100

    Style added

--- src/components/Herobanner.js
+++ src/components/Herobanner.js
@@ -6,8 +6,8 @@ const Herobanner = props => {
     <Box sx={{mt: {lg:'212px', xs:'70px'}, ml:{sm:'50px'}}} position='relative' p='20px'>
       <Typography color='#ff2625' fontWeight='600' fontSize='26px'>Fitness Club</Typography>
       <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'40px'}}}>Sweat, Smile <br/> and Repeat</Typography>
-    <Typography fontSize='20px' lineHeight='35px' >Check out the most effective exercises</Typography>
-    <Button variant='contained' color='error'>Explore Exercise</Button>
+    <Typography fontSize='20px' lineHeight='35px' mb={3} >Check out the most effective exercises</Typography>
+    <Button variant='contained' color='error' href='#exercses'>Explore Exercise</Button>
     </Box>
   )
 }
