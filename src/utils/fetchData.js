aec9b1db1746c3bc4b3f28a5490cf83f8ff707e2
Puspita <puspitagoswami14@gmail.com>
2023-06-20 22:40:18 +0100

    Rapid API key added

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,13 @@
+
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '6674b2cac1msh09f79213f696183p161236jsna84cc147558a',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+
 export const fetchData = async (url, options) =>{
     const response = await fetch(url, options);
     const data = await response.json();
